2c68bb3637ab0675ee3b0170056f41b923c03662
cacacai <jiashengcai97@gmail.com>
2022-01-16 22:17:59 +0800

    代码控制流、循环

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,43 @@ fn main() {
     //test_var();
     //test_scalar();
     //test_compound_types();
-    let  result = test_fn(23333,true);
-    println!("fn comeback {}",result);
+    //let  result = test_fn(23333,true);
+    //println!("fn comeback {}",result);
+    test_control_flow();
+}
+// 测试使用控制流
+fn test_control_flow() {
+    let num = 3;
+    if num > 5 {//必须为bool值，或者判断条件
+        println!("{}", num);
+    } else {
+        println!("{}", num);
+    }
+
+    //
+    let condition = true;
+    let number = if condition { 5 } else { 6 };//控制语句中没有分号，整体为一个表达式
+    println!("{}", number);
+
+
+    loop {
+        println!("loop 无限循环");
+        break;
+    }
+
+    let mut i = 0;
+    while i < 4 {
+        println!("{}", i);
+        i = i + 1;
+    }
+
+    let a = [1, 2, 3, 4, 5];
+    for element in a {
+        println!("{}",element);
+    }
+    for i in (20..29).rev() {//反转数组
+        println!("rev {}",i);
+    }
 }
 
 fn test_fn(x:i32,y:bool)-> i32{
