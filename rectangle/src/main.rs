79fc332c9640982f4c85c4a4a2d5225ddbe20ca2
cacacai <jiashengcai97@gmail.com>
2022-01-18 00:27:27 +0800

    定义方法，语块

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,4 +1,19 @@
-
+//添加注解为了直接调试，打印对象
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+// 实现自身的解构体，添加自身的方法函数
+impl Rectangle {
+    fn area(&self) -> u32 {//self 传入自身对象，类型于this访问；使用&表示借用，不可变
+        self.width * self.width
+    }
+    //比较整体大小
+    fn can_hold(&self,other:&Rectangle) -> bool {
+        self.width >other.width && self.height > other.height
+    }
+}
 
 fn main() {
     //let width = 30;
@@ -7,21 +22,22 @@ fn main() {
     //let rect1 = (30, 50);
     //println!("area {}",area_mate(rect1));
 
-    let rect1 = Rectangle{
-        width:30,height:20
-    };
+    let rect1 = Rectangle { width: 30, height: 20 };
     //println!("area {}",area_struct(&rect1));
-    println!("area {:?}",rect1);
+    //println!("area {:?}",rect1);
+    //println!("area {}",rect1.area());
+    let rect1 = Rectangle { width: 30, height: 20 };
+    let rect2 = Rectangle { width: 40, height: 20 };
+    let rect3 = Rectangle { width: 50, height: 60 };
+    println!("rect1 > rect2? {}", rect1.can_hold(&rect2));
+    println!("rect3 > rect2? {}", rect3.can_hold(&rect2));
 
 
 
 }
-//添加注解为了直接调试，打印对象
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32
-}
+
+
+
 //通过结构体计算，同时使用 & 借用，引用方式
 fn area_struct(rect:&Rectangle) -> u32 {
     rect.height * rect.width
